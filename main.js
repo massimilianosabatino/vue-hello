6d0ac5c97166fc3383863655efb79e1eb4f6be68
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-13 18:18:58 +0100

    add - button generate random image

--- main.js
+++ main.js
@@ -5,10 +5,22 @@ const { createApp } = Vue;
 
 createApp({
     //Root component
+    //Value for app instance binding
     data() {
         return {
             h1Content: 'Questo è sempre legato alla proprietà creata',
             imageSrc: 'https://picsum.photos/400/300'
         };
+    },
+    //Function used in the instance
+    methods: {
+        generateRandom(min, max){
+            return Math.floor(Math.random() * (max - min + 1) ) + min; 
+        },
+        changeSize(){
+            const min = this.generateRandom(100, 800);
+            const max = this.generateRandom(100, 700);
+            this.imageSrc = `https://picsum.photos/${min}/${max}`;
+        }
     }
 }).mount('#app'); //Mounting app (metod)
