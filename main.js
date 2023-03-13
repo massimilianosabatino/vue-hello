1e80265d4b801ae1346138ecdc554316516cd5ca
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-13 17:56:02 +0100

    add - image with src bound to app property

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ createApp({
     //Root component
     data() {
         return {
-            h1Content: 'Questo è sempre legato alla proprietà creata'
+            h1Content: 'Questo è sempre legato alla proprietà creata',
+            imageSrc: 'https://picsum.photos/400/300'
         };
     }
 }).mount('#app'); //Mounting app (metod)
